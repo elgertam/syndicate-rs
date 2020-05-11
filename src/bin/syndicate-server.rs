87bcaacaa4d7fb82a19bd429ab829de8173b4366
Tony Garnock-Jones <tonyg@leastfixedpoint.com>
2020-05-11 20:51:04 +0200

    Split out binary from library

--- src/bin/syndicate-server.rs
+++ src/bin/syndicate-server.rs
@@ -1,34 +1,7 @@
-#![recursion_limit="512"]
+use syndicate::{peer, spaces};
 
-mod bag;
-mod dataspace;
-mod packets;
-mod peer;
-mod skeleton;
-mod spaces;
-
-use preserves::value;
 use std::sync::{Mutex, Arc};
 use tokio::net::TcpListener;
-
-use std::sync::atomic::{AtomicUsize, Ordering};
-
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub enum Syndicate {
-    Placeholder(usize),
-}
-
-impl value::Domain for Syndicate {}
-
-static NEXT_PLACEHOLDER: AtomicUsize = AtomicUsize::new(0);
-impl Syndicate {
-    pub fn new_placeholder() -> Self {
-        Self::Placeholder(NEXT_PLACEHOLDER.fetch_add(1, Ordering::SeqCst))
-    }
-}
-
-pub type ConnId = u64;
-pub type V = value::ArcValue<Syndicate>;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+#![recursion_limit="512"]
+
+pub mod bag;
+pub mod dataspace;
+pub mod packets;
+pub mod peer;
+pub mod skeleton;
+pub mod spaces;
+
+pub use preserves::value;
+
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum Syndicate {
+    Placeholder(usize),
+}
+
+impl value::Domain for Syndicate {}
+
+static NEXT_PLACEHOLDER: AtomicUsize = AtomicUsize::new(0);
+impl Syndicate {
+    pub fn new_placeholder() -> Self {
+        Self::Placeholder(NEXT_PLACEHOLDER.fetch_add(1, Ordering::SeqCst))
+    }
+}
+
+pub type ConnId = u64;
+pub type V = value::ArcValue<Syndicate>;
