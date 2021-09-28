fe7086b84b1444a1c091520602a1017fb9ff2524
Tony Garnock-Jones <tonyg@leastfixedpoint.com>
2021-09-28 15:18:33 +0200

    More debug in counter.rs

--- syndicate-server/src/counter.rs
+++ syndicate-server/src/counter.rs
@@ -4,9 +4,13 @@ use syndicate::actor::*;
 
 pub fn adjust(t: &mut Activation, f: &Arc<Field<isize>>, delta: isize) {
     let f = f.clone();
+    tracing::trace!(v0 = ?t.get(&f), "adjust");
     *t.get_mut(&f) += delta;
+    tracing::trace!(v1 = ?t.get(&f), "adjust");
     t.on_stop(move |t| {
+        tracing::trace!(v0 = ?t.get(&f), "cleanup");
         *t.get_mut(&f) -= delta;
+        tracing::trace!(v1 = ?t.get(&f), "cleanup");
         Ok(())
     });
 }
@@ -14,7 +18,9 @@ pub fn adjust(t: &mut Activation, f: &Arc<Field<isize>>, delta: isize) {
 pub fn sync_and_adjust<M: 'static + Send>(t: &mut Activation, r: &Arc<Ref<M>>, f: &Arc<Field<isize>>, delta: isize) {
     let f = f.clone();
     let sync_handler = t.create(move |t: &mut Activation| {
+        tracing::trace!(v0 = ?t.get(&f), "sync");
         *t.get_mut(&f) += delta;
+        tracing::trace!(v1 = ?t.get(&f), "sync");
         Ok(())
     });
     t.sync(r, sync_handler)
