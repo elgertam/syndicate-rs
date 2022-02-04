fb31ea44cf6cb15bb29384a715d0e37900845640
Tony Garnock-Jones <tonyg@leastfixedpoint.com>
2022-02-04 17:06:18 +0100

    fixtags.sh

---
+++ fixtags.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+buildtag() {
+    name=$(grep '^name' "$1" | head -1 | sed -e 's:^.*"\([^"]*\)":\1:')
+    version=$(grep '^version' "$1" | head -1 | sed -e 's:^.*"\([^"]*\)":\1:')
+    echo "$name-v$version"
+}
+
+git tag "$(buildtag syndicate/Cargo.toml)"
+git tag "$(buildtag syndicate-macros/Cargo.toml)"
+git tag "$(buildtag syndicate-server/Cargo.toml)"
