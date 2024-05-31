b10acaf4a6e67f3b3c679b27fbc74db88ce2ec94
Tony Garnock-Jones <tonyg@leastfixedpoint.com>
2024-05-31 15:00:18 +0200

    Builder image for workflows

---
+++ .forgejo/workflows/builder-image/build-and-push.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# You need to have already logged in:
+#
+#     docker login git.syndicate-lang.org
+#
+# Use a token with read-only access to user scope, this seems to be sufficient (!)
+
+set -e
+docker build -t git.syndicate-lang.org/syndicate-lang/rust-builder .
+docker push git.syndicate-lang.org/syndicate-lang/rust-builder
