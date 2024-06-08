d1cb35dc760d583b02e95939fc717bfd085ec204
Tony Garnock-Jones <tonyg@leastfixedpoint.com>
2024-06-08 10:34:53 +0200

    Use internal copy of schema-bundle.bin for separate verifiability/publishability

--- syndicate-schema-plugin/build.rs
+++ syndicate-schema-plugin/build.rs
@@ -9,7 +9,7 @@ fn main() -> std::io::Result<()> {
     let mut c = CompilerConfig::new("crate::schemas".to_owned());
     c.add_external_module(ExternalModule::new(vec!["EntityRef".to_owned()], "crate::placeholder"));
 
-    let inputs = expand_inputs(&vec!["../syndicate/protocols/schema-bundle.bin".to_owned()])?;
+    let inputs = expand_inputs(&vec!["./schema-bundle.bin".to_owned()])?;
     c.load_schemas_and_bundles(&inputs, &vec![])?;
     compile(&c, &mut CodeCollector::files(gen_dir))
 }
