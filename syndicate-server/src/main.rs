e60a08e1bd84f0e6507f774edfe28d3812375b30
Tony Garnock-Jones <tonyg@leastfixedpoint.com>
2024-09-26 13:11:56 +0200

    Update Preserves version to 0.996

--- syndicate-server/src/main.rs
+++ syndicate-server/src/main.rs
@@ -91,7 +91,12 @@ async fn main() -> ActorResult {
         eprintln!(r"{}   \____{}/{}_/ {} /____/\__, /_/ /_/\____/_/\___/\__/_/\__/\___/", GREEN, BRIGHT_GREEN, GREEN, NORMAL);
         eprintln!(r"                  /____/");
         eprintln!(r"");
-        eprintln!(r" {}version {} [syndicate {}]{}", BRIGHT_YELLOW, env!("CARGO_PKG_VERSION"), syndicate::syndicate_package_version(), NORMAL);
+        eprintln!(r" {}version {} [syndicate {}, preserves {}]{}",
+                  BRIGHT_YELLOW,
+                  env!("CARGO_PKG_VERSION"),
+                  syndicate::syndicate_package_version(),
+                  syndicate::preserves::preserves_package_version(),
+                  NORMAL);
         eprintln!(r"");
         eprintln!(r" documentation & reference material: https://syndicate-lang.org/");
         eprintln!(r" source code & bugs: https://git.syndicate-lang.org/syndicate-lang/syndicate-rs");
